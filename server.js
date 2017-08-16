82a8c3001fae129ac3a08ca5ada0c893d6672b7e
Bryan Tumbaco <bryandario1994@gmail.com>
2017-08-15 20:37:50 -0500

    first

---
+++ server.js
@@ -0,0 +1,6 @@
+var SoapClient = require('node-soap-client').SoapClient;
+
+new SoapClient({wsdl: 'http://www.pttplc.com/webservice/pttinfo.asmx?WSDL'}
+	var searchingModule = new ptt.SearchingModule();
+
+});
